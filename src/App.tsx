7d47ac73d124ce3fbf388fe2d2bf9715c888d154
apelsinom <apelsinom@mail.ru>
2024-04-24 21:59:23 +0300

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./components/Site.module.css";
+import {Sneakers} from "./components/pages/Sneakers";
+import {Error404} from "./components/pages/Error404";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {ButtonLink} from "./components/pages/ButtonLink";
+import {Model} from "./components/pages/Model";
+import {PATH, sneakersArr} from "./DateCatalog/Catalog";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <ButtonLink address={PATH.PAGE1} firm={PATH.PAGE1.toUpperCase()}/>
+                    <ButtonLink address={PATH.PAGE2} firm={PATH.PAGE2.toUpperCase()}/>
+                    <ButtonLink address={PATH.PAGE3} firm={PATH.PAGE3.toUpperCase()}/>
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path="" element={<Navigate to={PATH.PAGE1} />}/>
+                        <Route path={`${PATH.PAGE1}/:id`} element={<Model firm={PATH.PAGE1}/>}/>
+                        <Route path={`${PATH.PAGE2}/:id`} element={<Model firm={PATH.PAGE2}/>}/>
+                        <Route path={`${PATH.PAGE3}/:id`} element={<Model firm={PATH.PAGE3}/>}/>
+                        <Route path={PATH.PAGE1} element={<Sneakers catalog={sneakersArr[PATH.PAGE1]} item={PATH.PAGE1.toUpperCase()}/>}/>
+                        <Route path={PATH.PAGE2} element={<Sneakers catalog={sneakersArr[PATH.PAGE2]} item={PATH.PAGE2.toUpperCase()}/>}/>
+                        <Route path={PATH.PAGE3} element={<Sneakers catalog={sneakersArr[PATH.PAGE3]} item={PATH.PAGE3.toUpperCase()}/>}/>
+                        {/*можно так, но остаётсЯ виден неправильный адрес*/}
+                        <Route path="*" element={<Error404 />}/>
+                        {/*так неправильный адрес затирается на 'error404'*/}
+                        {/*<Route path={PATH.ERROR} element={<Error404 />}/> 
+                        <Route path="*" element={<Navigate to={PATH.ERROR} />}/>*/}
+                    </Routes>
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
+        </div>
+    );
 }
+
 
 export default App;
